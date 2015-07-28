f7a0f6ad4bf186e5c5bd3ed3435c1e82c8c80c19
Jeremy Smith <goodanswerdesign@gmail.com>
2015-07-28 01:03:04 -0600

    Moved off-canvas to page template

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -57,49 +57,7 @@
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
   <?php print $page_top; ?>
-
-  <!-- Start of Off-canvas head -->
-<div class="off-canvas-wrap" data-offcanvas>
-  <div class="inner-wrap">
-    <nav class="tab-bar">
-      <section class="left-small">
-        <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
-      </section>
-
-      <section class="middle tab-bar-section">
-        <h1 class="title">Foundation</h1>
-      </section>
-
-      <section class="right-small">
-        <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
-      </section>
-    </nav>
-
-    <aside class="left-off-canvas-menu">
-      <ul class="off-canvas-list">
-        <li><label>Foundation</label></li>
-        <li><a href="#">The Psychohistorians</a></li>
-        <li><a href="#">...</a></li>
-      </ul>
-    </aside>
-
-    <aside class="right-off-canvas-menu">
-      <ul class="off-canvas-list">
-        <li><label>Users</label></li>
-        <li><a href="#">Hari Seldon</a></li>
-        <li><a href="#">...</a></li>
-      </ul>
-    </aside>
-
-    <section class="main-section">
-    <!-- End of off-canvas head -->
   <?php print $page; ?>
-  <!-- Start of off-canvas tail -->
-    </section>
-    <a class="exit-off-canvas"></a>
-    </div>
-    </div>
-    <!-- End of off-canvas tail -->
   <?php print $page_bottom; ?>
 </body>
 </html>
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -85,6 +85,44 @@
  * @see html.tpl.php
  */
 ?>
+<!-- Start of Off-canvas head -->
+<div class="off-canvas-wrap" data-offcanvas>
+  <div class="inner-wrap">
+    <nav class="tab-bar">
+      <section class="left-small">
+        <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+      </section>
+
+      <section class="middle tab-bar-section">
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+        <h1 class="title"><?php print $site_name; ?></h1>
+      </a>
+      </section>
+
+      <section class="right-small">
+        <a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
+      </section>
+    </nav>
+
+    <aside class="left-off-canvas-menu">
+      <ul class="off-canvas-list">
+        <li><label>Foundation</label></li>
+        <li><a href="#">The Psychohistorians</a></li>
+        <li><a href="#">...</a></li>
+      </ul>
+    </aside>
+
+    <aside class="right-off-canvas-menu">
+      <ul class="off-canvas-list">
+        <li><label>Users</label></li>
+        <li><a href="#">Hari Seldon</a></li>
+        <li><a href="#">...</a></li>
+      </ul>
+    </aside>
+
+    <section class="main-section">
+<!-- End of off-canvas head -->
+
 <div id="page-wrapper"><div id="page">
 
   <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
@@ -245,3 +283,10 @@
   </div></div> <!-- /.section, /#footer-wrapper -->
 
 </div></div> <!-- /#page, /#page-wrapper -->
+
+<!-- Start of off-canvas tail -->
+</section>
+<a class="exit-off-canvas"></a>
+</div>
+</div>
+<!-- End of off-canvas tail -->
